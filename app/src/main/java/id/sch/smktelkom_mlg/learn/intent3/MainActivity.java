cfb4e9a7bb4f47689e1891531c4209c6ac7e34e0
Zafira Candra <zafiracandrawulan@gmail.com>
2017-01-29 13:20:40 +0700

    Intent Browser

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -27,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.imageViewBrowser).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebPage("http://www.smktelkom-mlg.sch.id/");
+            }
+        });
+    }
+
+    public void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
     }
 
     public void composeSmsMessage(String message) {
